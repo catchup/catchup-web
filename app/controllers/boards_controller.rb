f74685705242977f7e1941badd7ac0ee24207d46
Antonio Scandurra <as-cii@outlook.com>
2014-07-01 19:34:21 +0200

    Scaffold Boards

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,29 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      redirect_to @board
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :boards
+
   root to: 'users#new'
 
   resources :users, only: [:create]
