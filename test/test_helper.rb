5abb0c35da7f75d2056ab6f1d194ffe8ddeee37d
Antonio Scandurra <as-cii@outlook.com>
2014-07-01 18:04:54 +0200

    Some foolprof I18n setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,13 +4,10 @@ require 'rails/test_help'
 require 'capybara/rails'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
 
 class ActionDispatch::IntegrationTest
-  # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
+  include AbstractController::Translation
 end
