b6a5f09ac79228f4052f01b7f556275e65286d35
Antonio Scandurra <as-cii@outlook.com>
2014-07-21 09:31:24 +0200

    Add Postmark API key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method   = :postmark
-  config.action_mailer.postmark_settings = { :api_key => "to add" }
+  config.action_mailer.postmark_settings = { api_key: "0b998566-50af-4efc-9d31-7f819a135691" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
