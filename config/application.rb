895cb6eb14a5a8904a0439f9d99dc1f42482e713
Antonio Scandurra <as-cii@outlook.com>
2014-07-21 09:40:10 +0200

    Supply a default host so that absolute URLs can work

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module TaskPlanner
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_mailer.default_url_options = { host: "task-planner.herokuapp.com" }
   end
 end
