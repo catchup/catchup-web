459ecdebe6c6071afba47c8ba8f6da8da1051663
Antonio Scandurra <as-cii@outlook.com>
2014-07-21 10:34:01 +0200

    Evaluate all the users every time, not just once

--- app/mailers/card_mailer.rb
+++ app/mailers/card_mailer.rb
@@ -1,14 +1,14 @@
 class CardMailer < ActionMailer::Base
-  default(
-    from: "asd@pittle.org",
-    to: User.all.pluck(:email)
-  )
+  default(from: "asd@pittle.org")
 
   def new_card(card)
     @card  = card
     @board = card.list.board
 
-    mail(subject: "New card on #{@board.title}")
+    mail(
+      subject: "New card on #{@board.title}",
+      to: everyone
+    )
   end
 
   def card_moved(card)
@@ -16,6 +16,15 @@ class CardMailer < ActionMailer::Base
     @board = @list.board
     @card  = card
 
-    mail(subject: "Card moved on #{@board.title}")
+    mail(
+      subject: "Card moved on #{@board.title}",
+      to: everyone
+    )
+  end
+
+  private
+
+  def everyone
+    User.all.pluck(:email)
   end
 end
