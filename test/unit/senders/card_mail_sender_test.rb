f94875b026ed024c30586874e5f9eb629049c341
Antonio Scandurra <me@as-cii.com>
2014-10-23 12:18:46 +0200

    Put unit tests under test/unit

--- test/unit/senders/card_mail_sender_test.rb
+++ test/unit/senders/card_mail_sender_test.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 require "minitest/mock"
 
-class CardMailNotifierTest < ActionMailer::TestCase
+class CardMailSenderTest < ActionMailer::TestCase
   test "sends an e-mail when a card is moved into another list" do
     cool_feature.stub(:changed_list?, true) do
-      CardMailNotifier.card_moved(cool_feature, nil)
+      CardMailSender.card_moved(cool_feature, nil)
     end
 
     assert delivered_email?
@@ -12,7 +12,7 @@ class CardMailNotifierTest < ActionMailer::TestCase
 
   test "does not send any e-mail when a card is not moved into another list" do
     cool_feature.stub(:changed_list?, false) do
-      CardMailNotifier.card_moved(cool_feature, nil)
+      CardMailSender.card_moved(cool_feature, nil)
     end
 
     refute delivered_email?
