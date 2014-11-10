813a7681d0e9429f6de299a3738d29c4d5400080
Antonio Scandurra <me@as-cii.com>
2014-11-10 16:42:17 +0100

    Use ENV variable to authenticate early adopters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :signed_in?
 
-  # Keep out of our demo! :)
-  http_basic_authenticate_with(
-    name: ENV.fetch("LOGIN_NAME"),
-    password: ENV.fetch("LOGIN_PASSWORD")
-  ) if Rails.env.production?
+  before_action :authenticate_early_adopters
 
   def current_user
     @current_user ||= User.find_by(email: session[:current_user])
@@ -14,5 +10,12 @@ class ApplicationController < ActionController::Base
 
   def signed_in?
     current_user != nil
+  end
+
+  def authenticate_early_adopters
+    early_adopters = JSON.parse ENV.fetch("EARLY_ADOPTERS")
+    authenticate_or_request_with_http_basic do |username, password|
+      early_adopters[username] == password
+    end
   end
 end
