65ed37251543ca86e9fb166012f57b6106687d99
Antonio Scandurra <me@as-cii.com>
2014-11-11 10:48:12 +0100

    Expose early adopter name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :signed_in?
+  helper_method :current_user, :signed_in?, :early_adopter_name
 
   before_action :authenticate_early_adopters if Rails.env.production?
 
@@ -10,6 +10,11 @@ class ApplicationController < ActionController::Base
 
   def signed_in?
     current_user != nil
+  end
+
+  def early_adopter_name
+    username, _ = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
+    username.capitalize
   end
 
   def authenticate_early_adopters
