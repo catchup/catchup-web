98d170ba90dca677a8c82b3d2e1bde72e854e1d8
Antonio Scandurra <me@as-cii.com>
2014-12-10 12:00:41 +0100

    Use the correct I18n.t label

--- test/integration/mock_authentication_test.rb
+++ test/integration/mock_authentication_test.rb
@@ -1,12 +1,10 @@
 require 'test_helper'
+require "pages/authentication_page"
 
 class MockAuthenticationTest < ActionDispatch::IntegrationTest
+  include AuthenticationPage
+
   test 'User authenticates via an arbitrary e-mail' do
-    visit root_url
-
-    fill_in 'user[email]', with: 'as-cii@me.com'
-    click_on t('users.new.login')
-
-    assert page.has_text? "as-cii@me.com"
+    sign_in users(:antonio)
   end
 end
