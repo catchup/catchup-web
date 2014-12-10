6f243a8cfd39f8156c97fae1bb64cd8758a08653
Antonio Scandurra <me@as-cii.com>
2014-12-10 12:02:21 +0100

    Use double-quoted strings on test [ci skip]

--- test/integration/mock_authentication_test.rb
+++ test/integration/mock_authentication_test.rb
@@ -4,7 +4,7 @@ require "pages/authentication_page"
 class MockAuthenticationTest < ActionDispatch::IntegrationTest
   include AuthenticationPage
 
-  test 'User authenticates via an arbitrary e-mail' do
+  test "User authenticates via an arbitrary e-mail" do
     sign_in users(:antonio)
   end
 end
