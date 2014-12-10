c99a0390b90d121e10caca6c67d3bd833b7f72e1
Antonio Scandurra <me@as-cii.com>
2014-12-10 11:49:22 +0100

    Add ownership concepts to User and Board

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   validates :email, presence: true
   validates_email_format_of :email
+
+  has_and_belongs_to_many :owned_boards,
+                          join_table: "boards_owners",
+                          class_name: "Board"
+
+  def create_board(params)
+    Board.create(params).tap { |b| b.add_owner(self) }
+  end
 end
