fe5000c34c56595363161a669c2428ae7d58febd
Antonio Scandurra <me@as-cii.com>
2014-12-11 10:11:40 +0100

    Deprecate early_adopter_name, use User#first_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   attr_reader :current_user
-  helper_method :current_user, :signed_in?, :early_adopter_name
+  helper_method :current_user, :signed_in?
 
   before_action :authenticate
 
   def signed_in?
     current_user != nil
-  end
-
-  def early_adopter_name
-    # TODO: place an attribute on User. e.g. User#first_name
-    username, _ = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
-    username.capitalize
   end
 
   def authenticate
