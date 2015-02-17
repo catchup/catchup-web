9d8659b9f88208dbf869849c4f39f82e2a3f8f65
Antonio Scandurra <me@as-cii.com>
2015-02-17 11:13:11 +0100

    Make card title editable

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :boards, only: [:create, :index, :show, :update] do
     match :toggle_subscription, on: :member, via: [:patch, :put]
 
-    resources :cards, only: [:create, :show] do
+    resources :cards, only: [:create, :show, :update] do
       match :move, on: :member, via: [:patch, :put]
       match :archive, on: :member, via: [:patch, :put]
     end
