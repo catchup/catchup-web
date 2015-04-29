56a9cfd6a5b91b353aa8a37f1e5c57bb63fd6246
Ali Servet Donmez <asd@pittle.org>
2015-04-29 02:01:39 +0200

    Boards page's :art: is virtually there
    There are various issues and overall design is lacking polishing and all of the rich client side interactions.
    
    More commits will follow.

--- app/controllers/new_boards_controller.rb
+++ app/controllers/new_boards_controller.rb
@@ -1,5 +1,38 @@
 class NewBoardsController < ApplicationController
   skip_before_action :authenticate
 
-  def index; end
+  def index
+    @boards = [
+      board("exalted/dotfiles", [ali]),
+      board("catchup/catchup-web", [antonio, ali, antonio, ali, ali, antonio]),
+      board("as-cii/terminal-emojify", [antonio, ali])
+    ]
+  end
+
+  private
+
+  def board(title, collaborators)
+    Board.new do |board|
+      board.title = title
+      board.collaborators = collaborators
+    end
+  end
+
+  def ali
+    user("314398")
+  end
+
+  def antonio
+    user("482957")
+  end
+
+  def user(id)
+    User.new do |user|
+      user.avatar_url = avatar_url(id)
+    end
+  end
+
+  def avatar_url(id)
+    "https://avatars3.githubusercontent.com/u/#{id}?v=3&s=460"
+  end
 end
